e8298022eea12e486053fa7a5cc85cbde4cb9267
Justin Hamerly <suvimountain@icloud.com>
2021-07-21 11:22:47 -0700

    new css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+// -------------------- GLOBAL FUNCTIONS --------------------//
+const Seattle = new StoreLocation('SEATTLE', 23, 65, 6.3);
+const Tokyo = new StoreLocation('TOKYO', 3, 24, 1.2);
+const Dubai = new StoreLocation('DUBAI', 11, 38, 3.7);
+const Paris = new StoreLocation('PARIS', 20, 38, 2.3);
+const Lima = new StoreLocation('LIMA', 2, 16, 4.6);
+
+const locArray = [Seattle, Tokyo, Dubai, Paris, Lima];
+
+const hoursArray = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM'];
+
+const locDivElement = document.getElementById('locations');
+
+// -------------------- CONSTRUCTOR FUNCTION --------------------//
 function StoreLocation(locationName, minCustHour, maxCustHour, avgPerCust){
   this.locationName = locationName;
   this.minCustHour = minCustHour;
@@ -8,15 +22,15 @@ function StoreLocation(locationName, minCustHour, maxCustHour, avgPerCust){
   this.storeResults = [];
 }
 
-const Seattle = new StoreLocation('SEATTLE', 23, 65, 6.3);
-const Tokyo = new StoreLocation('TOKYO', 3, 24, 1.2);
-const Dubai = new StoreLocation('DUBAI', 11, 38, 3.7);
-const Paris = new StoreLocation('PARIS', 20, 38, 2.3);
-const Lima = new StoreLocation('LIMA', 2, 16, 4.6);
-
-const locArray = [Seattle, Tokyo, Dubai, Paris, Lima];
-
-const hoursArray = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM'];
+// -------------------- CONSTUCTOR TOOLS --------------------//
+StoreLocation.prototype.renderRow = function(tbodyElem){
+  const trElem = newElement('tr', tbodyElem, null);
+  newElement('th', trElem, this.locationName);
+  for (let i=0; i < this.cookiesPerHour.length; i++){
+    newElement('td', trElem, this.cookiesPerHour[i]);
+  }
+  newElement('td', trElem, this.totalCookies);
+};
 
 StoreLocation.prototype.genData = function(){
   this.totalCookies = 0;
@@ -28,6 +42,7 @@ StoreLocation.prototype.genData = function(){
   }
 };
 
+// -------------------- GLOBAL FUNCTIONS --------------------//
 function randCustPerHour(a, b) {
   const customersPerHour = Math.floor(Math.random() * (b - a + 1) + a);
   return customersPerHour;
@@ -42,15 +57,13 @@ function newElement(tagname, parent, text){
   return element;
 }
 
-const locDivElement = document.getElementById('locations');
-
 function renderTable(){
   const tableElem = newElement('table', locDivElement, null);
   const theadElem = newElement('thead', tableElem, null);
   const trElem = newElement('tr', theadElem, null);
   newElement('th', trElem, null);
   for (let i=0; i < hoursArray.length; i++){
-    newElement('th', trElem, hoursArray[i]);
+    newElement('td', trElem, hoursArray[i]);
   }
   newElement('th', trElem, 'DAILY LOCATION TOTAL');
 
@@ -61,15 +74,6 @@ function renderTable(){
 
   getTableFooter(tableElem);
 }
-
-StoreLocation.prototype.renderRow = function(tbodyElem){
-  const trElem = newElement('tr', tbodyElem, null);
-  newElement('th', trElem, this.locationName);
-  for (let i=0; i < this.cookiesPerHour.length; i++){
-    newElement('td', trElem, this.cookiesPerHour[i]);
-  }
-  newElement('td', trElem, this.totalCookies);
-};
 
 function getTableFooter(tableElem){
   const tfootElem = newElement('tfoot', tableElem, null);
@@ -84,19 +88,25 @@ function getTableFooter(tableElem){
   }
 }
 
-// function renderStore(location) {
-//   console.log(location);
-//   const articleElem = newElement('article', locDivElement, null);
-//   newElement('h3', articleElem, location.locationName);
-//   const ulElem = newElement('ul', articleElem, null);
-//   for (let i = 0; i < location.storeResults.length; i++){
-//     newElement('li', ulElem, location.storeResults[i]);
-//   }
-// }
+function renderStore(location) {
+  console.log(location);
+  const articleElem = newElement('article', locDivElement, null);
+  newElement('h3', articleElem, location.locationName);
+  const ulElem = newElement('ul', articleElem, null);
+  for (let i = 0; i < location.storeResults.length; i++){
+    newElement('li', ulElem, location.storeResults[i]);
+  }
+}
 
+// -------------------- CALL FUNCTIONS --------------------//
 for (let i = 0; i < locArray.length; i++){
   locArray[i].genData();
-  // renderStore(locArray[i]);
 }
 
 renderTable();
+
+for (let i = 0; i < locArray.length; i++){
+  renderStore(locArray[i]);
+}
+
+
